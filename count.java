7e861d75fa96f09cd9b0414adb1b30070efffa58
TaoXingyu <48848952+TaoXingyu@users.noreply.github.com>
2020-08-17 09:23:24 -0600

    Add files via upload

---
+++ count.java
@@ -0,0 +1,12 @@
+
+public class count {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int count = 0;
+		while (count < 10) 
+		System.out.println("Welcome to Java");
+		count++;
+	}
+
+}
